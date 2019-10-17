4f09b06a9522396df6df73498b55a392c128f425
emir <emir.jumaliev@gmail.com>
2019-10-17 10:45:44 -0400

    was modified article_view

--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -1,74 +1,106 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from webapp.forms import ArticleForm
+from django.db.models import Q
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
+from django.views.generic import ListView, DetailView, CreateView,\
+    UpdateView, DeleteView
+
+from webapp.forms import ArticleForm, ArticleCommentForm, SimpleSearchForm
 from webapp.models import Article
-from django.views import View
-from django.views.generic import TemplateView
-class IndexView(TemplateView):
-    template_name = 'Article/index.html'
+from django.core.paginator import Paginator
+
+
+class IndexView(ListView):
+    template_name = 'article/index.html'
+    context_object_name = 'articles'
     model = Article
-    context_key = 'article'
+    ordering = ['-created_at']
+    paginate_by = 5
+    paginate_orphans = 1
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
 
-class ArticleView(TemplateView):
-    template_name = 'Article/article.html'
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(
+                Q(title__icontains=self.search_value)
+                | Q(author__icontains=self.search_value)
+            )
+        return queryset
 
-    def get_context_data(self, **kwargs):
-        pk = kwargs.get('pk')
-        context = super().get_context_data(**kwargs)
-        context['article'] = get_object_or_404(Article, pk=pk)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        context['archived_articles'] = self.get_archived_articles()
         return context
 
+    def get_archived_articles(self):
+        queryset = super().get_queryset()
+        return queryset
 
-class ArticleCreateView(View):
-    def get(self, request, *args, **kwargs):
-        form = ArticleForm()
-        return render(request, 'Article/create.html', context={'form': form})
+    def get_search_form(self):
+        return SimpleSearchForm(data=self.request.GET)
 
-    def post(self, request, *args, **kwargs):
-        form = ArticleForm(data=request.POST)
-        if form.is_valid():
-            article = Article.objects.create(
-                title=form.cleaned_data['title'],
-                author=form.cleaned_data['author'],
-                text=form.cleaned_data['text'],
-                category=form.cleaned_data['category']
-            )
-            return redirect('article_view', pk=article.pk)
-        else:
-            return render(request, 'Article/create.html', context={'form': form})
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
-class ArticleUpdateView(View):
-    def get(self, request, *args, **kwargs):
-        article = get_object_or_404(Article, pk=kwargs.get('pk'))
-        form = ArticleForm(data={
-            'title': article.title,
-            'author': article.author,
-            'text': article.text,
-            'category': article.category_id
-        })
-        return render(request, 'Article/update.html', context={'form': form, 'article': article})
+class ArticleView(DetailView):
+    template_name = 'article/article.html'
+    model = Article
 
-    def post(self, request, *args, **kwargs):
-        article = get_object_or_404(Article, pk=kwargs.get('pk'))
-        form = ArticleForm(data=request.POST)
-        if form.is_valid():
-            article.title = form.cleaned_data['title']
-            article.author = form.cleaned_data['author']
-            article.text = form.cleaned_data['text']
-            article.category = form.cleaned_data['category']
-            article.save()
-            return redirect('article_view', pk=article.pk)
-        else:
-            return render(request, 'Article/update.html', context={'form': form, 'article': article})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article = self.object
+        context['form'] = ArticleCommentForm()
+        comments = article.comments.order_by('-created_at')
+        self.paginate_comments_to_context(comments, context)
+        return context
+
+    def paginate_comments_to_context(self, comments, context):
+        paginator = Paginator(comments, 3, 0)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['comments'] = page.object_list
+        context['is_paginated'] = page.has_other_pages()
 
 
-class ArticleDeleteView(View):
-    def get(self, request, *args, **kwargs):
-        article = get_object_or_404(Article, pk=kwargs.get('pk'))
-        return render(request, 'Article/delete.html', context={'article': article})
+class ArticleCreateView(CreateView):
+    form_class = ArticleForm
+    model = Article
+    template_name = 'article/create.html'
 
-    def post(self, request, *args, **kwargs):
-        article = get_object_or_404(Article, pk=kwargs.get('pk'))
-        article.delete()
-        return redirect('index')
+    def get_success_url(self):
+        return reverse('article_view', kwargs={'pk': self.object.pk})
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article/update.html'
+    form_class = ArticleForm
+    context_object_name = 'article'
+
+    def get_success_url(self):
+        return reverse('article_view', kwargs={'pk': self.object.pk})
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article/delete.html'
+    context_object_name = 'article'
+    success_url = reverse_lazy('index')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.save()
+        return redirect(self.get_success_url())
