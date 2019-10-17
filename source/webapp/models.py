233ee771c3d7cb728090518583e3543da6e54b21
emir <emir.jumaliev@gmail.com>
2019-10-17 10:36:11 -0400

    was made migration

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Категория',
                                  related_name='articles')
+    
 
     def __str__(self):
         return self.title
@@ -31,4 +32,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31, verbose_name='Тег')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
